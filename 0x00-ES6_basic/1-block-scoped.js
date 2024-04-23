0345be2a5081fdb64a34d9741ed2bbbec49df461
khalid boumaghdar <khalidboumaghdar07@gmail.com>
2024-04-23 08:57:26 -0700

    khalid

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-   const task = false;
-   const task2 = true;
-	  if (task && task2) {
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
       return [task, task2];
     }
   }
