0ff87f70fa9e744f0f8712881cad550fcf318a75
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2024-08-20 17:55:32 +0000

    nodejs

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,16 @@
-console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on('data', (data) => {
-	const name = data.toString().trim();
-	console.log(`Your name is: ${name}`);
-	console.log("This important software is now closing");
-	process.exit();
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
