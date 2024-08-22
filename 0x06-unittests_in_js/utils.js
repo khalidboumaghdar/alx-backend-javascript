d187617c00eb3403fd7fd7ed67642b6b79e3ab63
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2024-08-22 20:56:52 +0000

    Using stubs allows you to greatly speed up your test. When executing thousands of tests, saving a few seconds is important Using stubs allows you to control specific edge case (e.g a function throwing an error or returning a specific result like a number or a timestamp)

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,11 +1,16 @@
-// utils.js
 const Utils = {
-    calculateNumber: function (type, a, b) {
-        if (type === 'SUM') {
-            return Math.round(a) + Math.round(b);
-        }
-        // You can add more operations like SUBTRACT, DIVIDE if needed
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
     }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+    }
+    return 0;
+  },
 };
 
 module.exports = Utils;
