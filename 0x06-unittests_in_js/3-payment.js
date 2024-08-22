fd6d58705c9769252f34dea9607bdee78e1a5b5e
khalidboumaghdar <khalidboumaghdar07@gmail.com>
2024-08-22 20:55:39 +0000

    Using stubs allows you to greatly speed up your test. When executing thousands of tests, saving a few seconds is important Using stubs allows you to control specific edge case (e.g a function throwing an error or returning a specific result like a number or a timestamp)

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,9 +1,8 @@
-// 3-payment.js
 const Utils = require('./utils');
 
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    const result = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: ${result}`);
-}
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const totalCost = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${totalCost}`);
+};
 
 module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,16 +1,15 @@
-// 3-payment.test.js
 const sinon = require('sinon');
-const { expect } = require('chai');
 const Utils = require('./utils');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 
-describe('sendPaymentRequestToApi', function() {
-    it('should call Utils.calculateNumber with the correct arguments', function() {
-        const spy = sinon.spy(Utils, 'calculateNumber');
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
+    const bigBrother = sinon.spy(Utils);
 
-        sendPaymentRequestToApi(100, 20);
-
-        expect(spy.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-        spy.restore();
-    });
+    sendPaymentRequestToApi(100, 20);
+    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
+    bigBrother.calculateNumber.restore();
+  });
 });
